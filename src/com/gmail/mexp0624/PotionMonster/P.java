b8066ae4043031ea4c73b93c06100475819caab3
cs8425 <cs8425@gmail.com>
2019-01-21 00:20:37 +0800

    some clean up

--- src/com/gmail/mexp0624/PotionMonster/P.java
+++ src/com/gmail/mexp0624/PotionMonster/P.java
@@ -14,9 +14,6 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
-//import org.bukkit.entity.Skeleton;
-//import org.bukkit.entity.WitherSkeleton;
-//import org.bukkit.entity.Skeleton.SkeletonType;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
@@ -35,22 +32,22 @@ public class P extends JavaPlugin implements Listener
 	public static P pl;
 	FileConfiguration config;
 	Random random = new Random();
-	HashMap<String, List<effect>> affect = new HashMap();
-	HashMap<String, Integer> respawn = new HashMap();
-  
+	HashMap<EntityType, List<effect>> affect = new HashMap();
+	HashMap<EntityType, Integer> respawn = new HashMap();
+
 	public void onEnable() {
 		pl = this;
 		Bukkit.getPluginManager().registerEvents(this, this);
 
 		loadConfig();
 	}
-  
+
 	public void onDisable() {
 		HandlerList.unregisterAll((org.bukkit.plugin.java.JavaPlugin)pl);
 		this.affect.clear();
 		this.respawn.clear();
 	}
-  
+
 	public void loadConfig() {
 		reloadConfig();
 		File configFile = new File(getDataFolder(), "config.yml");
@@ -65,15 +62,13 @@ public class P extends JavaPlugin implements Listener
 		int j = (arrayOfEntityType = EntityType.values()).length;
 		for (int i = 0; i < j; i++) {
 			EntityType ent = arrayOfEntityType[i];
-			String mob = getMob(ent);
-			if (mob != null) {
-				System.out.println("[addType]: " + mob);
-				this.affect.put(mob, parseList(mob));
-				this.respawn.put(mob, Integer.valueOf(this.config.getInt(mob + ".respawn", 0)));
-			}
+			String mob = ent.name();
+			System.out.println("[addType]: " + mob);
+			this.affect.put(ent, parseList(mob));
+			this.respawn.put(ent, Integer.valueOf(this.config.getInt(mob + ".respawn", 0)));
 		}
 	}
-  
+
 	List<effect> parseList(String mobName) {
 		List<String> tmplist = this.config.getStringList(mobName + ".conf");
 		List<effect> mobEff = new ArrayList();
@@ -85,7 +80,7 @@ public class P extends JavaPlugin implements Listener
 		}
 		return mobEff;
 	}
-  
+
 	effect parseLine(String str) {
 		String[] effs = str.split(":");
 
@@ -138,55 +133,7 @@ public class P extends JavaPlugin implements Listener
 		}
 		return out;
 	}
-  
-	public String getMob(EntityType e) {
-		switch (e) {
-		case ZOMBIE: 
-			return "ZOMBIE";
-		case CREEPER: 
-			return "CREEPER";
-		case SKELETON: 
-			return "SKELETON";
-		case WITHER_SKELETON:
-			return "WITHER_SKELETON";
-		case SPIDER: 
-			return "SPIDER";
-		case CAVE_SPIDER: 
-			return "CAVE_SPIDER";
-		case SLIME: 
-			return "SLIME";
-		case WITCH: 
-			return "WITCH";
-		case ENDERMAN: 
-			return "ENDERMAN";
-		case PIG_ZOMBIE: 
-			return "PIG_ZOMBIE";
-		case BLAZE: 
-			return "BLAZE";
-		case ENDER_DRAGON: 
-			return "ENDER_DRAGON";
-		case GHAST: 
-			return "GHAST";
-		case GUARDIAN: 
-			return "GUARDIAN";
-		case SILVERFISH: 
-			return "SILVERFISH";
-		case WITHER: 
-			return "WITHER";
-		case WITHER_SKULL: 
-			return "WITHER_SKULL";
-		case OCELOT: 
-			return "OCELOT";
-		case WOLF: 
-			return "WOLF";
-		case IRON_GOLEM: 
-			return "IRON_GOLEM";
-		case SNOWMAN: 
-			return "SNOWMAN";
-		}
-		return null;
-	}
-  
+
 	LivingEntity transType(LivingEntity ent, EntityType et, byte type) {
 		boolean val = false;
 		if (type == 'T') { // toggle
@@ -240,10 +187,10 @@ public class P extends JavaPlugin implements Listener
 		}
 		return ent;
 	}
-  
+
 	@EventHandler
 	public void onSpawn(CreatureSpawnEvent e) {
-		String mob = getMob(e.getEntityType());
+		EntityType mob = e.getEntityType();
 		LivingEntity ent = e.getEntity();
 		if (mob != null) {
 			List<effect> efflist = (List)this.affect.get(mob);
@@ -264,10 +211,10 @@ public class P extends JavaPlugin implements Listener
 			}
 		}
 	}
-  
+
 	@EventHandler
 	public void onMobDeath(EntityDeathEvent e) {
-		String mob = getMob(e.getEntityType());
+		EntityType mob = e.getEntityType();
 		if (mob != null) {
 			Integer p = (Integer)this.respawn.get(mob);
 			if ((p != null) && (p.intValue() > this.random.nextInt(10000))) {
@@ -283,7 +230,7 @@ public class P extends JavaPlugin implements Listener
 			}
 		}
 	}
-  
+
 	static class effect {
 		public final short P;
 		public final List<PotionEffect> eff;
